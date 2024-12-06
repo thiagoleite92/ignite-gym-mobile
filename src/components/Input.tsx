7c330afcd25423cf30365cf6019a3b903e9a3458
Thiago Leite <leite.tjs@gmail.com>
2024-12-05 23:10:43 -0300

    feat: forms validation sign up

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,31 +1,56 @@
-import { Input as GlueStackInput, InputField } from '@gluestack-ui/themed'
+import {
+  Input as GlueStackInput,
+  InputField,
+  FormControl,
+  FormControlErrorText,
+  FormControlError,
+} from '@gluestack-ui/themed'
 import { ComponentProps } from 'react'
 
 type InputProps = ComponentProps<typeof InputField> & {
   isReadOnly?: boolean
+  errorMessage?: string | null
+  isInvalid?: boolean
 }
 
-export function Input({ isReadOnly = false, ...rest }: InputProps) {
+export function Input({
+  errorMessage = null,
+  isInvalid = false,
+  isReadOnly = false,
+  ...rest
+}: InputProps) {
+  const invalid = !!errorMessage || isInvalid
+
   return (
-    <GlueStackInput
-      h="$14"
-      borderWidth="$0"
-      borderRadius="$md"
-      $focus={{
-        borderWidth: 1,
-        borderColor: '$green500',
-      }}
-      isReadOnly={isReadOnly}
-      opacity={isReadOnly ? 0.5 : 1}
-    >
-      <InputField
-        px="$4"
-        bg="$gray700"
-        color="$white"
-        fontFamily="$body"
-        placeholderTextColor="$gray300"
-        {...rest}
-      />
-    </GlueStackInput>
+    <FormControl isInvalid={invalid} w="$full" mb="$4">
+      <GlueStackInput
+        h="$14"
+        borderWidth="$0"
+        borderRadius="$md"
+        $focus={{
+          borderWidth: 1,
+          borderColor: invalid ? '$red600' : '$green500',
+        }}
+        isReadOnly={isReadOnly}
+        opacity={isReadOnly ? 0.5 : 1}
+        isInvalid={isInvalid}
+        $invalid={{
+          borderWidth: 1,
+          borderColor: '$red600',
+        }}
+      >
+        <InputField
+          px="$4"
+          bg="$gray700"
+          color="$white"
+          fontFamily="$body"
+          placeholderTextColor="$gray300"
+          {...rest}
+        />
+      </GlueStackInput>
+      <FormControlError>
+        <FormControlErrorText>{errorMessage}</FormControlErrorText>
+      </FormControlError>
+    </FormControl>
   )
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -6,6 +6,9 @@ import {
   Heading,
   ScrollView,
 } from '@gluestack-ui/themed'
+import { useForm, Controller } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 
 import BackgroundImg from '@assets/background.png'
 import Logo from '@assets/logo.svg'
@@ -14,11 +17,55 @@ import { Button } from '@components/Button'
 import { useNavigation } from '@react-navigation/native'
 import { AuthNavigatorRouterProps } from '@routes/auth.routes'
 
+type SignUpFormProps = {
+  name: string
+  email: string
+  password: string
+  password_confirm: string
+}
+
+const signUpSchema = yup.object({
+  name: yup.string().required('Informe o nome'),
+  email: yup.string().email('E-mail inválido').required('Informe o e-mail'),
+  password: yup
+    .string()
+    .min(6, 'Deve ter no mínimo 6 caracteres')
+    .max(10, 'Deve ter no máximo 10 caracteres')
+    .required('Informe a senha'),
+  password_confirm: yup
+    .string()
+    .oneOf([yup.ref('password'), ''], 'Senhas não conferem')
+    .required('Confirme a senha'),
+})
+
 export function SignUp() {
   const { navigate } = useNavigation<AuthNavigatorRouterProps>()
 
   const handleGoToLogin = () => {
     navigate('sign-in')
+  }
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpFormProps>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      password_confirm: '',
+    },
+    resolver: yupResolver(signUpSchema),
+  })
+
+  const handleSignUp = ({
+    email,
+    name,
+    password,
+    password_confirm: passwordConfirm,
+  }: SignUpFormProps) => {
+    console.log(email, name, password, passwordConfirm)
   }
 
   return (
@@ -44,15 +91,71 @@ export function SignUp() {
           </Center>
           <Center flex={1} gap="$2">
             <Heading color="$gray100">Crie sua conta</Heading>
-            <Input placeholder="Nome" />
-            <Input
-              placeholder="E-mail"
-              keyboardType="email-address"
-              autoCapitalize="none"
+
+            <Controller
+              control={control}
+              name="name"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Nome"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors?.name?.message}
+                />
+              )}
             />
-            <Input placeholder="Senha" secureTextEntry />
-            <Button title="Criar e acessar" />
+
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="E-mail"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors?.email?.message}
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Senha"
+                  secureTextEntry
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors?.password?.message}
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="password_confirm"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Confirmar Senha"
+                  secureTextEntry
+                  onChangeText={onChange}
+                  value={value}
+                  onSubmitEditing={handleSubmit(handleSignUp)}
+                  returnKeyType="send"
+                  errorMessage={errors?.password_confirm?.message}
+                />
+              )}
+            />
+
+            <Button
+              title="Criar e acessar"
+              onPress={handleSubmit(handleSignUp)}
+            />
           </Center>
+
           <Button
             title="Voltar para o login"
             variant="outline"
